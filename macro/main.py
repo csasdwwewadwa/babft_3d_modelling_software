3a7cfd13dd53f063cc992db30374104beaad5431
csasdwwewadwa <csasdwwewadwa2@gmail.com>
2025-12-20 08:24:24 +0700

    world to screen builtin to Workspace

--- macro/main.py
+++ macro/main.py
@@ -8,6 +8,8 @@ import math
 import numpy as np
 from scipy.spatial.transform import Rotation
 
+from typing import Optional, Literal
+
 from core import Vector3, look_at
 from config import SCREEN_RESOLUTION
 
@@ -18,9 +20,9 @@ class Camera:
     #   |       | [_____/
     #   |       |]=V*‾‾‾
     #   |_______|    ^
-    #       ^        | Camera focal point
+    #       ^        | actual view origin
     #       | <---->
-    #       |offset_z
+    #       | offset_z
     #       |
     #     origin
     
@@ -44,16 +46,21 @@ class Camera:
     #              506
 
 
-    def __init__(self, pos:Vector3, rot:Rotation, hotkey:str=None):
-        self.pos = pos
-        self.rot = rot
-        self.rot_inv = rot.inv()
+    def __init__(self, position:Vector3, rotation:Rotation, hotkey:str=None):
+        self.pos = position
+        self.rot = rotation
+        self.rot_inv = rotation.inv()
 
         self.hotkey = hotkey
 
         self.offset_z = 126/41   #  ~3.07    |  111 - (100/(1-5.2/70.8))
         self.offset = Vector3(0, 0, -self.offset_z)
         self.screen_z = 31625/41 #  ~771.34  |  506/70.8 * (100/(1-5.2/70.8))
+
+    def to_screen(self, position:Vector3):
+        local_pos = self._world_to_local(position)
+        screen_pos = self._local_to_screen(local_pos)
+        return (round(screen_pos.x), round(screen_pos.y))
     
     def _local_to_screen(self, local_pos:Vector3) -> Vector3:
         screen_pos = local_pos * (self.screen_z / local_pos.z)
@@ -66,21 +73,52 @@ class Camera:
         local_pos = (pos - self.pos).rotate(self.rot_inv) + self.offset
         return local_pos
     
-    def to_screen(self, pos:Vector3):
-        local_pos = self._world_to_local(pos)
-        screen_pos = self._local_to_screen(local_pos)
-        return (round(screen_pos.x), round(screen_pos.y))
-    
 
 class Workspace:
     def __init__(self):
         self.workspace_size = Vector3(84, 84, 84)
         self.center = Vector3(0, 42, 0)
+        self.block_placement_pos = Vector3(-54, 0, 54)
+
+        self.DEBOUNCE = 0.1 # seconds
 
         self.cameras:list[Camera] = self._get_cameras()
+        self.current_camera:Camera = None
         # for cam in self.cameras:
         #     print(cam.hotkey, cam.pos)
 
+    # ---- the sauce ----
+    def place(position:Vector3, rotation:Rotation, scale: Vector3):
+        pass
+
+    # ---- actions submacro ----
+    def click_world(self, position:Vector3, direction:Optional[Literal['U','D','Up','Down']]=None):
+        screen_coord = self.current_camera.to_screen(position)
+        ahk.click(*screen_coord, direction=direction)
+        time.sleep(self.DEBOUNCE)
+
+    def mouse_move_world(self, position:Vector3):
+        screen_coord = self.current_camera.to_screen(position)
+        ahk.mouse_move(*screen_coord)
+        time.sleep(self.DEBOUNCE)
+
+    def drag_world(self, position_from:Vector3, position_to:Vector3):
+        self.click_world(position_from, 'Down')
+        self.click_world(position_to, "Up")
+
+    def switch_camera(self, camera:Camera):
+        if self.current_camera is None:
+            ahk.key_press('m')
+            time.sleep(self.DEBOUNCE)
+
+        if camera == self.current_camera:
+            return
+        
+        self.current_camera = camera
+        ahk.key_press(camera.hotkey)
+        time.sleep(self.DEBOUNCE)
+
+    # ---- init ----
     def _get_cameras(self):
         cameras:list[Camera] = []
         hotkey_seq = self._hotkey_seq()
@@ -114,8 +152,6 @@ class Workspace:
         for k in 'qertyupghjklzxcvbnm':
             yield k
 
-
-
 if __name__ == '__main__':
     workspace = Workspace()
 
@@ -123,8 +159,7 @@ if __name__ == '__main__':
     time.sleep(3)
 
     for camera in workspace.cameras:
-        ahk.key_press(camera.hotkey)
-        p = Vector3(30, 0, 30)
-        screen_coord = camera.to_screen(p)
-        ahk.mouse_move(*screen_coord)
+        workspace.switch_camera(camera)
+        p = workspace.block_placement_pos
+        workspace.mouse_move_world(p)
         time.sleep(1)
